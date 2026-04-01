92712c8a7cd1b9956d40ac693d82984cdc43aa26
bibliotherapy <bibliother@naver.com>
2026-04-01 10:56:23 +0900

    Auto-backup: src/features/movement_quality.py (2026-04-01 10:56:23)

---
+++ src/features/movement_quality.py
@@ -0,0 +1,5 @@
+"""GMFCS-E&R movement quality descriptors.
+
+Extracts clinically-meaningful movement quality features aligned with
+GMFCS-E&R assessment criteria for each of the 5 movement types.
+"""
