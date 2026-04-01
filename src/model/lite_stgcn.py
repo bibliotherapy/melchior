7bdcbf7b9bd5aaba387922fb4c93aaa6b9e96175
bibliotherapy <bibliother@naver.com>
2026-04-01 10:56:30 +0900

    Auto-backup: src/model/lite_stgcn.py (2026-04-01 10:56:30)

---
+++ src/model/lite_stgcn.py
@@ -0,0 +1,5 @@
+"""Lightweight Spatial-Temporal Graph Convolutional Network.
+
+3-layer ST-GCN (64 channels, ~200K params) operating on raw 3D skeleton
+input (T, 17, 3). Produces a 128D embedding per clip.
+"""
