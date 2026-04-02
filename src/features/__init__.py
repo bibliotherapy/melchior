573b45e9ebd9dfcbb97ec02f1e3ebfc203ac66b6
bibliotherapy <bibliother@naver.com>
2026-04-02 11:24:47 +0900

    Auto-backup: src/features/__init__.py (2026-04-02 11:24:47)

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,1 +1,1 @@
-"""Feature extraction: skeleton features, interaction features, context vector."""
+"""Feature extraction: skeleton features, interaction features, context vector, walker spatial features."""
