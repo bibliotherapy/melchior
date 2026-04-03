05ec38830712e62d9e98a078f0a08468ace5a16a
bibliotherapy <bibliother@naver.com>
2026-04-03 11:51:49 +0900

    Auto-backup: src/utils/naming.py (2026-04-03 11:51:49)

--- src/utils/naming.py
+++ src/utils/naming.py
@@ -8,11 +8,21 @@ def clip_id_to_patient(clip_id):
         'kku_w_01_FV' -> 'kku'
         'ajy_cr_02_LV' -> 'ajy'
         'hdi_c_s_01_RV' -> 'hdi'
+        'hdi_cc_s_01_FV' -> 'hdi'
+        'hdi_s_cc_01_FV' -> 'hdi'
     """
     parts = clip_id.split("_")
-    movement_codes = {"w", "cr", "c", "s", "sr"}
+    simple_codes = {"w", "cr", "sr"}
+    compound_codes = {"c_s", "s_c", "cc_s", "s_cc"}
+
     for i, part in enumerate(parts):
-        if part in movement_codes:
+        # Check 2-part compound codes first (higher priority)
+        if i + 1 < len(parts):
+            compound = f"{part}_{parts[i + 1]}"
+            if compound in compound_codes:
+                return "_".join(parts[:i])
+        # Then check simple codes
+        if part in simple_codes:
             return "_".join(parts[:i])
     return parts[0]
 
